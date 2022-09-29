997b09ff017a3a97292de61c91dde07a2cf6eb66
Jesus Cordoba <jessusscj@gmail.com>
2022-09-28 20:10:26 -0500

    Se agrega proyecto calculadora

---
+++ script.js
@@ -0,0 +1,46 @@
+function capturaNumeroUno(){
+    let numero1 = Number(document.getElementById("numero1").value);
+    return numero1;
+}
+
+function capturaNumeroDos(){
+    let numero2 = Number(document.getElementById("numero2").value);
+    return numero2;
+}
+
+
+function suma(){
+    let numero1 = capturaNumeroUno();
+    let numero2 = capturaNumeroDos();
+    let suma = numero1 + numero2;
+    document.getElementById("resultado").innerHTML = `<div class="alert alert-success" id="resultado" role="alert">
+          ${suma}              
+    </div>`;
+}
+
+function resta(){
+    let numero1 = capturaNumeroUno();
+    let numero2 = capturaNumeroDos();
+    let resta = numero1 - numero2;
+    document.getElementById("resultado").innerHTML = `<div class="alert alert-success" id="resultado" role="alert">
+    ${resta}              
+</div>`;
+}
+
+function multiplicacion(){
+    let numero1 = capturaNumeroUno();
+    let numero2 = capturaNumeroDos();
+    let multiplicacion = numero1 * numero2;
+    document.getElementById("resultado").innerHTML = `<div class="alert alert-success" id="resultado" role="alert">
+    ${multiplicacion}              
+</div>`;
+}
+
+function division(){
+    let numero1 = capturaNumeroUno();
+    let numero2 = capturaNumeroDos();
+    let division = Number(numero1.value) / Number(numero2.value);
+    document.getElementById("resultado").innerHTML = `<div class="alert alert-success" id="resultado" role="alert">
+    ${division}              
+</div>`;
+}
